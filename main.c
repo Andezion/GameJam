27ecf5d6b9f42cfd2e2fc868d5e19b42c91fe2d3
Andezion <245122@edu.p.lodz.pl>
2024-05-10 18:03:52 +0300

    Тимурчегг топ

--- main.c
+++ main.c
@@ -1,10 +1,51 @@
 #include <stdio.h>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_audio.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+int SIZE = 800;
+
 int main(int argc, char *argv[])
 {
-    printf("Hello, World!\n");
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("Knight", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SIZE, SIZE, SDL_WINDOW_RESIZABLE);
+    if (window == NULL)
+    {
+        printf("Error in creating window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL)
+    {
+        printf("Error in creating renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Event event;
+
+    int running = 1;
+    while(running)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_q)
+            {
+                running = 0;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
