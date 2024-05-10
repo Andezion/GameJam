6d5e7e811e14afd1019a113b4f9cea4186197c25
Andezion <245122@edu.p.lodz.pl>
2024-05-10 18:46:01 +0300

    Тимурчегг топ

--- main.c
+++ main.c
@@ -1,22 +1,136 @@
 #include <stdio.h>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_audio.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <time.h>
+#include <SDL.h>
 
-int SIZE = 800;
+int HEIGHT = 800;
+int WIDTH = 600;
+
+void set_background(SDL_Renderer *renderer)
+{
+    /*int size = 25;
+    SDL_Rect cell;
+    cell.w = size;
+    cell.h = size;
+
+    for(int i = 0; i <= 32; i++)
+    {
+        for(int j = 0; j <= 24; j++)
+        {
+            cell.x = 800 + i * size;
+            cell.y = 600 + j * size;
+            
+            int type = 1 + rand() % 6;
+            if(i == 0 || j == 0)
+            {
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderFillRect(renderer, &cell);
+            }
+            else
+            {
+                if(type == 1)
+                {
+                    SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+                if(type == 2)
+                {
+                    SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+                if(type == 3)
+                {
+                    SDL_SetRenderDrawColor(renderer, 150, 150, 150, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+                if(type == 4)
+                {
+                    SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+                if(type == 5)
+                {
+                    SDL_SetRenderDrawColor(renderer, 250, 250, 250, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+                if(type == 6)
+                {
+                    SDL_SetRenderDrawColor(renderer, 70, 70, 70, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+            }
+        }
+    }*/
+    int cell_size = 20;
+
+
+    SDL_Rect cell;
+    cell.w = cell_size;
+    cell.h = cell_size;
+
+    for(int i = 0; i < 40; i++)
+    {
+        for(int j = 0; j < 30; j++)
+        {
+            cell.x = i * cell_size;
+            cell.y = j * cell_size;
+
+            int pick = 1 + rand() % 6;
+
+            if(i == 0 || j == 0 || i == 39 || j == 29)
+            {
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                SDL_RenderFillRect(renderer, &cell);
+            }
+            else
+            {
+                if(pick == 1)
+                {
+                    SDL_SetRenderDrawColor(renderer, 13, 28, 43, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+                else if(pick == 2)
+                {
+                    SDL_SetRenderDrawColor(renderer, 43, 33, 3, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+                else if(pick == 3)
+                {
+                    SDL_SetRenderDrawColor(renderer, 12, 25, 11, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+                else if(pick == 4)
+                {
+                    SDL_SetRenderDrawColor(renderer, 11, 19, 25, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+                else if(pick == 5)
+                {
+                    SDL_SetRenderDrawColor(renderer, 20, 16, 25, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+                else
+                {
+                    SDL_SetRenderDrawColor(renderer, 25, 16, 20, 255);
+                    SDL_RenderFillRect(renderer, &cell);
+                }
+            }
+
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderDrawRect(renderer, &cell);
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("Knight", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SIZE, SIZE, SDL_WINDOW_RESIZABLE);
+    SDL_Window *window = SDL_CreateWindow("Knight", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, HEIGHT, WIDTH, SDL_WINDOW_RESIZABLE);
     if (window == NULL)
     {
         printf("Error in creating window: %s\n", SDL_GetError());
         return 1;
     }
-
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == NULL)
     {
@@ -40,6 +154,11 @@ int main(int argc, char *argv[])
         }
 
         SDL_RenderClear(renderer);
+        set_background(renderer);
+
+        SDL_Rect rectangle = {200, 200, 100, 100};
+        SDL_RenderFillRect(renderer, &rectangle);
+
         SDL_RenderPresent(renderer);
     }
 
