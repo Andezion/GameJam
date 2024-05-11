9d73768f8dc3f3c812c0fad24c9a5acf68916ec1
BeliyAndrey <aboba@ya.uu>
2024-05-11 14:54:25 +0200

    Restart суууука

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL_image.h>
-#include <SDL2/SDL_ttf.h>
+#include <SDL_ttf.h>
 #include "chort.h"
 #include "man.h"
 
@@ -208,179 +208,172 @@ void drawGameOver(SDL_Renderer *renderer, SDL_Texture *gameOver, SDL_Texture *re
 
 int main(int argc, char *argv[])
 {
-    SDL_Rect bases[] = {{50, 50, 50, 50},
-                        {700, 50, 50, 50},
-                        {50, 500, 50, 50},
-                        {700, 500, 50, 50}};
-    SDL_Init(SDL_INIT_EVERYTHING);
-    IMG_Init(IMG_INIT_PNG);
-    TTF_Init();
-    enum STATE state = mainMenu;
-    SDL_ShowCursor(SDL_DISABLE);
+    do {
+        SDL_Rect bases[] = {{50,  50,  50, 50},
+                            {700, 50,  50, 50},
+                            {50,  500, 50, 50},
+                            {700, 500, 50, 50}};
+        SDL_Init(SDL_INIT_EVERYTHING);
+        IMG_Init(IMG_INIT_PNG);
+        TTF_Init();
+        int restartFlag = 0;
+        enum STATE state = mainMenu;
+        SDL_ShowCursor(SDL_DISABLE);
 
-    int pick[40][30];
-    for(int i = 0; i < 40; i++)
-    {
-        for(int j = 0; j < 30; j++)
-        {
-            int num = 1 + rand() % 6;
-            pick[i][j] = num;
+        int pick[40][30];
+        for (int i = 0; i < 40; i++) {
+            for (int j = 0; j < 30; j++) {
+                int num = 1 + rand() % 6;
+                pick[i][j] = num;
+            }
         }
-    }
 
-    struct chort_t cherti[10] = {0};
-    struct man_t mane[10] = {0};
-    int man_num = 4;
-    int cherti_num = 4;
-    int chortAmount;
-    SDL_Window *window = SDL_CreateWindow("GameJam", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, HEIGHT, WIDTH, SDL_WINDOW_OPENGL);
-    if (window == NULL)
-    {
-        printf("Error in creating window: %s\n", SDL_GetError());
-        return 1;
-    }
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == NULL)
-    {
-        printf("Error in creating renderer: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
+        struct chort_t cherti[10] = {0};
+        struct man_t mane[10] = {0};
+        int man_num = 4;
+        int cherti_num = 4;
+        int chortAmount;
+        SDL_Window *window = SDL_CreateWindow("GameJam", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, HEIGHT, WIDTH,
+                                              SDL_WINDOW_OPENGL);
+        if (window == NULL) {
+            printf("Error in creating window: %s\n", SDL_GetError());
+            return 1;
+        }
+        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        if (renderer == NULL) {
+            printf("Error in creating renderer: %s\n", SDL_GetError());
+            SDL_DestroyWindow(window);
 
-        SDL_Quit();
-        return 1;
-    }
+            SDL_Quit();
+            return 1;
+        }
 
-    for (int i = 0; i < cherti_num; ++i) {
-        cherti[i] = spawn_chort(bases[rand() % 4]);
-    }
-
-    for(int i = 0; i < man_num; i++)
-    {
-        mane[i] = spawn_man(bases, rand() % 4);
-    }
-
-    // Загружаем Изображения
-    SDL_Texture *cursorTexture = NULL;
-    SDL_Texture *logo = NULL;
-    SDL_Texture *start = NULL;
-    SDL_Texture *cat = NULL;
-    SDL_Texture *gameOverScreen = NULL;
-    SDL_Texture *restarScreen = NULL;
-    loadTexture(&cursorTexture, renderer, "../Sprites/Coursor.png");
-    loadTexture(&logo, renderer, "../Sprites/Team Logo.png");
-    loadTexture(&start, renderer, "../Sprites/StartGame.png");
-    loadTexture(&cat, renderer, "../Sprites/cat.png");
-    loadTexture(&gameOverScreen, renderer, "../Sprites/GameOver.png");
-    loadTexture(&restarScreen, renderer, "../Sprites/PressRToRestart.png");
-
-
-    SDL_Event event;
-
-    int running = 1;
-    while(running)
-    {
         for (int i = 0; i < cherti_num; ++i) {
-            if (cherti[i].dead && ((rand() % 100) == 0)) {
-                ochko_andreja += cherti[i].speed * 5;
-                cherti[i] = spawn_chort(bases[rand() % 4]);
-            }
+            cherti[i] = spawn_chort(bases[rand() % 4]);
         }
 
-        for(int i = 0; i < man_num; i++)
-        {
-            if(mane[i].dead && ((rand() % 100) == 0))
-            {
-                mane[i] = spawn_man(bases, rand() & 4);
-            }
-        }
-        int mouse_pressed = 0;
-        while(SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT )
-            {
-                running = 0;
-            }
-            if(event.type == SDL_MOUSEBUTTONUP && state == mainMenu)
-            {
-                int x, y;
-                SDL_GetMouseState(&x, &y);
-                if (x > 160 && x < 640 && y > 100 && y < 132)
-                {
-                    state = game;
-                    timeStart = SDL_GetTicks();
-                }
-            }
-            if (event.type == SDL_MOUSEBUTTONDOWN) {
-                mouse_pressed = 1;
-            }
-
+        for (int i = 0; i < man_num; i++) {
+            mane[i] = spawn_man(bases, rand() % 4);
         }
 
-        // Ебля со временем
-        unsigned long long currentTime = SDL_GetTicks();
-        if(currentTime - timeFromSpawn > 2500)
-        {
+        // Загружаем Изображения
+        SDL_Texture *cursorTexture = NULL;
+        SDL_Texture *logo = NULL;
+        SDL_Texture *start = NULL;
+        SDL_Texture *cat = NULL;
+        SDL_Texture *gameOverScreen = NULL;
+        SDL_Texture *restarScreen = NULL;
+        loadTexture(&cursorTexture, renderer, "../Sprites/Coursor.png");
+        loadTexture(&logo, renderer, "../Sprites/Team Logo.png");
+        loadTexture(&start, renderer, "../Sprites/StartGame.png");
+        loadTexture(&cat, renderer, "../Sprites/cat.png");
+        loadTexture(&gameOverScreen, renderer, "../Sprites/GameOver.png");
+        loadTexture(&restarScreen, renderer, "../Sprites/PressRToRestart.png");
 
-        }
 
+        SDL_Event event;
 
-        switch(state)
-        {
-            case mainMenu:
-                //state = game;
-                SDL_RenderClear(renderer);
-                drawMainMenu(renderer, logo, start, cat);
-                drawCoursor(renderer, cursorTexture);
-                SDL_RenderPresent(renderer);
-                break;
-            case game:
+        int running = 1;
+
+            while (running) {
                 for (int i = 0; i < cherti_num; ++i) {
-                    if (update_chort((SDL_Rect){350, 250, 80, 80}, cherti+i, mouse_pressed))
-                        state = gameOver;
-                }
-                for (int i = 0; i < man_num; ++i) {
-                   if (update_man((SDL_Rect){350, 250, 80, 80}, mane+i, mouse_pressed))
-                    {
-                        mane[i].dead = 1;
+                    if (cherti[i].dead && ((rand() % 100) == 0)) {
+                        ochko_andreja += cherti[i].speed * 5;
+                        cherti[i] = spawn_chort(bases[rand() % 4]);
                     }
                 }
 
-                SDL_RenderClear(renderer);
-                // Тут начало отрисовки
-                set_background(renderer, pick);
-                drawMainObjects(renderer);
-                for (int i = 0; i < cherti_num; ++i) {
-                    draw_chort(renderer, cherti+i);
+                for (int i = 0; i < man_num; i++) {
+                    if (mane[i].dead && ((rand() % 100) == 0)) {
+                        mane[i] = spawn_man(bases, rand() & 4);
+                    }
                 }
-                for(int i = 0; i < man_num; i++)
-                {
-                    draw_man(renderer, mane + i);
+                int mouse_pressed = 0;
+                while (SDL_PollEvent(&event)) {
+                    if (event.type == SDL_QUIT) {
+                        running = 0;
+                    }
+                    if (event.type == SDL_MOUSEBUTTONUP && state == mainMenu) {
+                        int x, y;
+                        SDL_GetMouseState(&x, &y);
+                        if (x > 160 && x < 640 && y > 100 && y < 132) {
+                            state = game;
+                            timeStart = SDL_GetTicks();
+                        }
+                    }
+                    if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_r && state == gameOver) {
+                        goto exit;
+                    }
+
+                    if (event.type == SDL_MOUSEBUTTONDOWN) {
+                        mouse_pressed = 1;
+                    }
+
                 }
-                drawCoursor(renderer, cursorTexture);
-                // Тут конец отрисовки
-                SDL_RenderPresent(renderer);
-                break;
-            case gameOver:
-                SDL_RenderClear(renderer);
-                drawGameOver(renderer, gameOverScreen, restarScreen);
-                SDL_RenderPresent(renderer);
-                SDL_Delay(2500);
-                goto exit;
-            default:
-                return 5;
-        }
-    }
 
-    exit:
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_DestroyTexture(cursorTexture);
-    SDL_DestroyTexture(logo);
-    SDL_DestroyTexture(start);
-    SDL_DestroyTexture(cat);
-    SDL_DestroyTexture(gameOverScreen);
-    SDL_DestroyTexture(restarScreen);
-    IMG_Quit();
-    SDL_Quit();
+                // Ебля со временем
+                unsigned long long currentTime = SDL_GetTicks();
+                if (currentTime - timeFromSpawn > 2500) {
 
-    return 0;
+                }
+
+
+                switch (state) {
+                    case mainMenu:
+                        //state = game;
+                        SDL_RenderClear(renderer);
+                        drawMainMenu(renderer, logo, start, cat);
+                        drawCoursor(renderer, cursorTexture);
+                        SDL_RenderPresent(renderer);
+                        break;
+                    case game:
+                        for (int i = 0; i < cherti_num; ++i) {
+                            if (update_chort((SDL_Rect) {350, 250, 80, 80}, cherti + i, mouse_pressed))
+                                state = gameOver;
+                        }
+                        for (int i = 0; i < man_num; ++i) {
+                            if (update_man((SDL_Rect) {350, 250, 80, 80}, mane + i, mouse_pressed)) {
+                                mane[i].dead = 1;
+                            }
+                        }
+
+                        SDL_RenderClear(renderer);
+                        // Тут начало отрисовки
+                        set_background(renderer, pick);
+                        drawMainObjects(renderer);
+                        for (int i = 0; i < cherti_num; ++i) {
+                            draw_chort(renderer, cherti + i);
+                        }
+                        for (int i = 0; i < man_num; i++) {
+                            draw_man(renderer, mane + i);
+                        }
+                        drawCoursor(renderer, cursorTexture);
+                        // Тут конец отрисовки
+                        SDL_RenderPresent(renderer);
+                        break;
+                    case gameOver:
+                        SDL_RenderClear(renderer);
+                        drawGameOver(renderer, gameOverScreen, restarScreen);
+                        SDL_RenderPresent(renderer);
+                        break;
+                    default:
+                        return 5;
+                }
+            }
+
+            exit:
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_DestroyTexture(cursorTexture);
+            SDL_DestroyTexture(logo);
+            SDL_DestroyTexture(start);
+            SDL_DestroyTexture(cat);
+            SDL_DestroyTexture(gameOverScreen);
+            SDL_DestroyTexture(restarScreen);
+            IMG_Quit();
+            SDL_Quit();
+            if (!running)
+                return 0;
+
+    }while(1);
 }
